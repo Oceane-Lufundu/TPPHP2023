0342ef49b59995d4143560ba06853ddc3fffae02
Oceane-Lufundu <121879631+Oceane-Lufundu@users.noreply.github.com>
2023-01-31 13:59:09 +0100

    modif du 3101 à 14h

--- PARTIEL_2023/Definition_Classes.php
+++ PARTIEL_2023/Definition_Classes.php
@@ -51,10 +51,7 @@ class Personne {
 
 	//Méthodes
 	private function afficher() {
-		$this->num . '<br/>';
-		$this->nom . '<br/>';
-		$this->prenom . '<br/>';
-		$this->entreprise->nom . '<br/>';
+
 	}
 
 	public calculerSalaire() {
